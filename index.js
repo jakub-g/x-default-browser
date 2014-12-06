7d429b5264eabb8e32994f492e47f0758fabdc31
jakub-g <jakub.g.opensource@gmail.com>
2014-12-06 20:40:20 +0100

    First release
    Tested on Win7, WinXP, Ubuntu 14.04

---
+++ index.js
@@ -0,0 +1,10 @@
+var detect;
+if (process.platform == 'win32') {
+    detect = require('./lib/detect-windows');
+} else if (process.platform == 'linux' || process.platform == 'freebsd') {
+    detect = require('./lib/detect-linux');
+} else {
+    detect = require('./lib/detect-stub');
+}
+
+module.exports = detect;
