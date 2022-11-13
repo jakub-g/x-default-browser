883a31625acf11069120b3fc85098ee5f4e4e84b
jakub-g <jakub.g.opensource@gmail.com>
2022-11-13 20:52:01 +0100

    fix macos detection following default-browser-id upgrade
    default-browser-id@2 uses promise API, not callback API

--- test/macos.js
+++ test/macos.js
@@ -1,64 +1,68 @@
-var assert = require('assert');
-var detect = require('rewire')('../lib/detect-mac');
+var assert = require("assert");
+var detect = require("rewire")("../lib/detect-mac");
 
 var defaultBrowserIdResponse = {
   err: undefined,
   browserId: undefined,
 };
 
-var defaultBrowserIdStub = function (cb) {
-  cb(defaultBrowserIdResponse.err, defaultBrowserIdResponse.browserId);
+var defaultBrowserIdStub = function () {
+  if (defaultBrowserIdResponse.err) {
+    return Promise.reject(defaultBrowserIdResponse.err);
+  } else {
+    return Promise.resolve(defaultBrowserIdResponse.browserId);
+  }
 };
 
-detect.__set__('defaultBrowserMac', defaultBrowserIdStub);
+detect.__set__("defaultBrowserMac", defaultBrowserIdStub);
 
-describe('macOS tests', function () {
+describe("macOS tests", function () {
   beforeEach(function () {
     defaultBrowserIdResponse.err = undefined;
     defaultBrowserIdResponse.browserId = undefined;
   });
 
-  it('detects chrome', function (done) {
-    defaultBrowserIdResponse.browserId = 'com.google.chrome';
+  it("detects chrome", function (done) {
+    defaultBrowserIdResponse.browserId = "com.google.chrome";
 
     detect(function (err, res) {
       assert.equal(res.isChrome, true);
       assert.equal(res.isChromium, false);
       assert.equal(res.isWebkit, true);
-      assert.equal(res.commonName, 'chrome');
+      assert.equal(res.commonName, "chrome");
       assert.equal(res.identity, defaultBrowserIdResponse.browserId);
       done(err);
     });
   });
 
-  it('detects opera', function (done) {
-    defaultBrowserIdResponse.browserId = 'com.operasoftware.Opera';
+  it("detects opera", function (done) {
+    defaultBrowserIdResponse.browserId = "com.operasoftware.Opera";
 
     detect(function (err, res) {
       assert.equal(res.isChrome, false);
       assert.equal(res.isChromium, false);
       assert.equal(res.isOpera, true);
       assert.equal(res.isWebkit, true);
-      assert.equal(res.commonName, 'opera');
+      assert.equal(res.commonName, "opera");
       assert.equal(res.identity, defaultBrowserIdResponse.browserId);
       done(err);
     });
   });
 
-  it('detects firefox', function (done) {
-    defaultBrowserIdResponse.browserId = 'org.mozilla.firefox';
+  it("detects firefox", function (done) {
+    defaultBrowserIdResponse.browserId = "org.mozilla.firefox";
 
     detect(function (err, res) {
       assert.equal(res.isFirefox, true);
       assert.equal(res.isWebkit, false);
-      assert.equal(res.commonName, 'firefox');
+      assert.equal(res.commonName, "firefox");
       assert.equal(res.identity, defaultBrowserIdResponse.browserId);
       done(err);
     });
   });
 
-  it('detects safari', function (done) {
-    defaultBrowserIdResponse.browserId = 'com.apple.Safari';
+  it("detects safari", function (done) {
+    defaultBrowserIdResponse.browserId = "com.apple.Safari";
 
     detect(function (err, res) {
       assert.equal(res.isSafari, true);
@@ -67,7 +71,7 @@ describe('macOS tests', function () {
       assert.equal(res.isOpera, false);
       assert.equal(res.isFirefox, false);
       assert.equal(res.isWebkit, true);
-      assert.equal(res.commonName, 'safari');
+      assert.equal(res.commonName, "safari");
       assert.equal(res.identity, defaultBrowserIdResponse.browserId);
       done(err);
     });
